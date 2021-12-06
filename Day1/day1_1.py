d55a39a19d5550203feb41ef0b66a1339f2acd85
Ciaran <irishrocker182@gmail.com>
2021-12-06 22:32:13 +0000

    Day 1 Completed
    Day 1 challenge completed

---
+++ Day1/day1_1.py
@@ -0,0 +1,19 @@
+lines = []
+with open('day1_1_input.txt') as f:
+    lines = f.readlines()
+
+current = 0;
+previous = 0;
+first = True;
+totalCount = 0
+for line in lines:
+    if(first):
+        current = int(line);
+        first = False;
+    else:
+        previous = current;
+        current = int(line)
+        if(current > previous):
+            totalCount += 1;
+
+print("Total increments : " + str(totalCount));
---
+++ Day1/day1_2.py
@@ -0,0 +1,27 @@
+lines = []
+lines_as_ints = []
+with open('day1_1_input.txt') as f:
+    lines = f.readlines()
+
+
+lines_as_ints = [int(x) for x in lines]
+
+currentSum = 0
+compareSum = 0
+totalCount = 0
+counter = 0;
+first = True;
+
+while (counter < (len(lines) - 2 )):
+    if(first):
+        currentSum = lines_as_ints[counter] + lines_as_ints[counter + 1] + lines_as_ints[counter + 2];
+        first = False;
+    else:
+        compareSum = currentSum
+        currentSum = lines_as_ints[counter] + lines_as_ints[counter + 1] + lines_as_ints[counter + 2];
+        if(currentSum > compareSum):
+            totalCount += 1;
+    counter += 1;
+
+print("Total increments : " + str(totalCount));
+    
